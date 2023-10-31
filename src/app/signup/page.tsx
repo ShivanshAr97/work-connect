a215a97239821671ba901dcb51e75eb2a204947e
ravinainn <nain.ravi12345@gmail.com>
2023-10-31 16:24:58 +0530

    changing signup css

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,71 +1,105 @@
-"use client"
+"use client";
 
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Signup() {
-  const router = useRouter()
+  const router = useRouter();
   const [user, setUser] = useState({
-    email:"",
-    password:""
-  })
-  const [disabled, setDisabled] = useState(true)
-  const [loading, setLoading] = useState(false)
+    email: "",
+    password: "",
+  });
+  const [disabled, setDisabled] = useState(true);
+  const [loading, setLoading] = useState(false);
 
-  
-  const onSignUp=async()=>{
+  const onSignUp = async () => {
     try {
-      setLoading(true)
-      const resp = await axios.post("/api/users/signup", user)
+      setLoading(true);
+      const resp = await axios.post("/api/users/signup", user);
       console.log(resp.data);
-      router.push('/profile')
-      toast.success(resp.data.message)
-    } catch (err:any) {
+      router.push("/profile");
+      toast.success(resp.data.message);
+    } catch (err: any) {
       console.log(err);
-      toast.error("Email address or password incorrect")
-    }finally{
-      setLoading(false)
+      toast.error("Email address or password incorrect");
+    } finally {
+      setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
-    if(user.email.length>0 && user.password.length>0){
-      setDisabled(false)
+    if (user.email.length > 0 && user.password.length > 0) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
     }
-    else{
-      setDisabled(true)
-    }
-  
-  }, [user])
-  
+  }, [user]);
+
   return (
-    <div className="m-12">
-        <h1 className="text-4xl mb-8">{loading?"Processing":"Sign Up"}</h1>
-        <hr />
-            <hr />
-            <div className="my-8">
-        <label htmlFor="name">Email: </label>
+    <div className="mx-80 my-10 p-20  rounded-lg bg-c1Light">
+      <h1 className="text-white font-bold text-4xl mb-8">
+        {loading ? "Processing" : "Sign Up"}
+      </h1>
+      <hr className="rounded-full" />
+
+      <div className="my-8">
+        <form action=""></form>
+        <label className="text-white text-lg" htmlFor="name">
+          Email:{" "}
+        </label>
         <br />
-        <input className="border-2 my-2 rounded-md outline-none px-2 py-1" placeholder='Enter email address' type="email" value={user.email} onChange={(e)=>setUser({...user, email:e.target.value})}/>
+        <input
+          className="text-white bg-c1 my-2 rounded-md outline-none px-4 py-2 placeholder:text-xs"
+          placeholder="Enter your Email"
+          type="email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
         <br />
         <br />
-        <label htmlFor="name">Password</label>
+        <label className="text-white text-lg" htmlFor="name">
+          Password
+        </label>
         <br />
-        <input className="border-2 my-2 rounded-md outline-none px-2 py-1" placeholder='Enter password' type="password" value={user.password} onChange={(e)=>setUser({...user, password:e.target.value})}/>
-        <Link href="/resetpassword" className='text-sm mx-12'>Forgot Password</Link>
+        <input
+          className="text-white bg-c1 my-2 rounded-md outline-none px-4 py-2 placeholder:text-xs"
+          placeholder="Enter Password"
+          type="password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
         <br />
-        <button className="border bg-red-500 mt-4 px-2 py-1 rounded-md font-medium" onClick={onSignUp}>Sign up</button>
-        </div>
-        <a className='px-6 py-3 text-white bg-violet-800 rounded-md ' href='/api/auth/signin'>Sign in using Linkedin</a>
+        <Link
+          href="/resetpassword"
+          className="text-c2 text-sm underline underline-offset-2"
+        >
+          Forgot Password
+        </Link>
+        <br />
+        <a
+          className="text-c2 text-sm underline underline-offset-2 "
+          href="/api/auth/signin"
+        >
+          Sign in using Linkedin
+        </a>
         <br />
         <br />
-        <Link href="/login" className='text-green-600 font-medium'>Log in instead</Link>
-        <br />
-        <ToastContainer />
+        <button
+          className=" text-base bg-c3 rounded-md mt-4 px-8 py-2 font-medium"
+          onClick={onSignUp}
+        >
+          Sign up
+        </button>
+      </div>
+      <Link href="/login" className="text-green-600 font-medium">
+        Log in instead
+      </Link>
+      <br />
+      <ToastContainer />
     </div>
-  )
+  );
 }
